ce0de44a0b78a9e0896d22a99ae099735ddc129c
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2023-06-11 04:20:29 +0100

    Added fake predictive text to the seach bar

--- application.js
+++ application.js
@@ -24,112 +24,29 @@ if (button) {
 
 // ----------------------------[ Suggestive text Search bar ]----------------------------
 
-const locations = [
-  {
-    name: 'Kintra St',
-    latitude: 55.856833,
-    longitude: -4.308500
-  },
-  {
-    name: 'The Field',
-    latitude: 55.859556,
-    longitude: -4.303972
-  },
-  {
-    name: 'Stag St',
-    latitude: 55.861651,
-    longitude: -4.301731
-  },
-  {
-    name:'Napier St',
-    latitude:55.861461,
-    longitude: -4.306350
-  }
-];
+const searchBar = document.getElementById("search-bar");
+const suggestionsList = document.querySelector(".suggestions-list");
 
-const postcodeInput = document.getElementById('search-bar');
-const suggestionsList = document.getElementById('suggestions');
-
-postcodeInput.addEventListener('input', function() {
-  console.log("Input detected.")
-  const postcode = postcodeInput.value.trim();
-
-  geocodePostcode(postcode, function(latitude, longitude) {
-    if (latitude !== null && longitude !== null) {
-      const closestLocation = findClosestLocation(latitude, longitude);
-      displaySuggestions([closestLocation]);
-    } else {
-      displaySuggestions([]);
-    }
-  });
-});
-
-function geocodePostcode(postcode, callback) {
-  const apiUrl = `https://nominatim.openstreetmap.org/search?q=${postcode}&format=json&addressdetails=1`;
-
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (Array.isArray(data) && data.length > 0) {
-        const { lat, lon } = data[0];
-        const latitude = parseFloat(lat);
-        const longitude = parseFloat(lon);
-        callback(latitude, longitude);
-      } else {
-        callback(null, null);
-      }
-    })
-    .catch(error => {
-      console.error('Error geocoding postcode:', error);
-      callback(null, null);
-    });
-}
-
-// Logic to find the closest location based on the latitude and longitude
-function findClosestLocation(latitude, longitude) {
-  let closestLocation = null;
-  let closestDistance = Number.MAX_VALUE;
-
-  for (const location of locations) {
-    const distance = calculateDistance(latitude, longitude, location.latitude, location.longitude);
-
-    if (distance < closestDistance) {
-      closestLocation = location;
-      closestDistance = distance;
-    }
-  }
-
-  return closestLocation;
-}
-
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Earth's radius in km
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c;
-  return distance;
-}
-
-function toRad(value) {
-  return value * Math.PI / 180;
-}
-
-function displaySuggestions(suggestions) {
-  suggestionsList.innerHTML = '';
-
-  if (suggestions.length === 0) {
-    const li = document.createElement('li');
-    li.textContent = 'No suggestions found';
-    suggestionsList.appendChild(li);
+searchBar.addEventListener("input", () => {
+  const searchText = searchBar.value.trim();
+  
+  if (searchText === "") {
+    suggestionsList.style.display = "none";
   } else {
-    suggestions.forEach(function(suggestion) {
-      const li = document.createElement('li');
-      li.textContent = suggestion.name;
+    const suggestedLocations = [
+      "Kintra St, Govan, Glasgow G51 2RL",
+      "Southcroft St, Govan, Glasgow G51 2DH",
+      "Stag St, Govan, Glasgow G51 2LS"
+    ];
+
+    suggestionsList.innerHTML = "";
+    suggestedLocations.forEach(location => {
+      const li = document.createElement("li");
+      li.textContent = location;
       suggestionsList.appendChild(li);
     });
+
+    suggestionsList.style.display = "block";
   }
-}
+});
+
